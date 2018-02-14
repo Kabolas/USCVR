45f62ac2ce324d886dd8cb07eb9c7a9361a9dc3d
Plocate <pierre-antoine.locatelli@u-psud.fr>
2018-02-14 12:16:04 +0100

    DÃplacement de la camÃra avec les gachettes is good.

--- Assets/SteamVR/InteractionSystem/Core/Scripts/Throwable.cs
+++ Assets/SteamVR/InteractionSystem/Core/Scripts/Throwable.cs
@@ -178,8 +178,8 @@ namespace Valve.VR.InteractionSystem
 			}
 			else
 			{
-				velocity = Player.instance.trackingOriginTransform.TransformVector( hand.controller.velocity );
-				angularVelocity = Player.instance.trackingOriginTransform.TransformVector( hand.controller.angularVelocity );
+				//velocity = Player.instance.trackingOriginTransform.TransformVector( hand.controller.velocity );
+				//angularVelocity = Player.instance.trackingOriginTransform.TransformVector( hand.controller.angularVelocity );
 				position = hand.transform.position;
 			}
 
